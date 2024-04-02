69bf89e69a8f9d8cc202101fc28e1c4efd942a84
Mehdi <mehdiennajehfr@gmail.com>
2024-04-02 17:45:46 +0200

    projet fini

--- recupInscription.php
+++ recupInscription.php
@@ -6,14 +6,6 @@ $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_STRING);
 $commentaire = filter_input(INPUT_POST, 'commentaire', FILTER_SANITIZE_STRING);
 
 #var_dump($_POST);
-echo '</br> Votre nom : ';
-echo $nom;
-echo '</br> Votre email : ';
-echo $email;
-echo '</br> Votre age : ';
-echo $age;
-echo '</br> Votre commentaire : ';
-echo $commentaire;
 
 if (empty($nom)) {
     echo 'Veuillez saisir un nom' . '</br>';
@@ -22,18 +14,22 @@ if (empty($nom)) {
         echo 'veuillez ne pas mettre de chiffres' . '</br>';
     } else {
         if (preg_match('/\d/', $nom)) {
-            echo '<br/> veuillez ne pas mettre de chiffres';
-        } 
+            echo '<br/> veuillez ne pas mettre de chiffres </br>';
+        } else {
+            echo '</br> Votre nom : ';echo $nom;
+        }
     }
 }
 if (empty($email)) {
     echo 'Veuillez saisir un email' . '</br>';
 } else {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo '<br> Veuillez saisir un email valide !';
+        echo '<br> Veuillez saisir un email valide !</br>';
     } else {
         if (strlen($email) >= 255) {
-            echo '<br> Veuillez saisir un email plus court';
+            echo '<br> Veuillez saisir un email plus court</br>';
+        } else {
+            echo '</br> Votre email : '; echo $email;
         }
     }
 }
@@ -41,14 +37,23 @@ if (empty($age)) {
     echo '<br> Veuillez saisir un age';
 } else 
     if (!filter_var($age, FILTER_VALIDATE_INT)) {
-        echo "<br> l'age n'a des chiffres";
+        echo "<br> l'age n'a des chiffres </br>";
 }else {
     if ($age < 18) {
-        echo"l'age ne peut pas être inferieur à 18";
+        echo"l'age ne peut pas être inferieur à 18 </br>";
+    } else {
+        echo '</br> Votre age : ';
+echo $age;
     }
 }
 
 if (empty($commentaire)) {
     echo 'Veuillez saisir un commentaire' . '</br>';
+} else {
+   
+echo '</br> Votre commentaire : ';
+echo $commentaire;
 }
+
+
 
